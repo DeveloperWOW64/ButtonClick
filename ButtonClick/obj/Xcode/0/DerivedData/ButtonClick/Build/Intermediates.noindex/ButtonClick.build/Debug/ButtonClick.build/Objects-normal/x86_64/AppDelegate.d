821eda9dedaba8631888323f2169f1356a1a23cc
DeveloperWOW64 <71598437+DeveloperWOW64@users.noreply.github.com>
2021-02-16 18:04:57 +0000

    Do too much stuff

---
+++ ButtonClick/obj/Xcode/0/DerivedData/ButtonClick/Build/Intermediates.noindex/ButtonClick.build/Debug/ButtonClick.build/Objects-normal/x86_64/AppDelegate.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Volumes/Code/Repositories/ButtonClick/ButtonClick/obj/Xcode/0/AppDelegate.m \
+  /Volumes/Code/Repositories/ButtonClick/ButtonClick/obj/Xcode/0/AppDelegate.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/usr/include/mach-o/dyld.modulemap
